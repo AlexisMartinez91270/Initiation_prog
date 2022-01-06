4a8efb9dbafc60ac042881cabd622d6635894d96
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-01-06 10:58:48 +0100

    Exo 2.1

--- C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
+++ C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
@@ -10,6 +10,17 @@ namespace ConsoleAppFramework
     {
         static void Main(string[] args)
         {
+            int a = 5;
+            // a = 5 ; b = - ; c = -
+            int b = 3;
+            // a = 5 ; b = 3 ; c = -
+            int c = a + b;
+            // a = 5 ; b = 3 ; c = 8
+            a = 2;
+            // a = 2 ; b = 3 ; c = 8
+            c = b - a;
+            // a = 2 ; b = 3 ; c = 1
+            Console.WriteLine(a, b, c); 
         }
     }
 }
