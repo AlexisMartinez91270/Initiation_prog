cecb8e61d8143789cb911204cbe37975495d3484
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-01-06 11:41:32 +0100

    Exo 2.7

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,10 @@
-let n, p, q;
+let n, p;
 //crée les entiers n, p et q
-n = 5;
-//affecte la valeur 5 dans la case mémoire de n
-p = 5;
+n = 10;
+//affecte la valeur 10 dans la case mémoire de n
+p = 4;
 //affecte la valeur 5 dans la case mémoire de p
-q = n / (n - p);
-//affecte la valeur de la division de la valeur de n par la valeur de la soustraction de n-p dans la case mémoire de p
+n = n * p;
+//affecte la valeur de la multiplication de n*p dans la case mémoire de n
+p = n / p;
+//affecte la valeur de la division de n/p dans la case mémoire de n
