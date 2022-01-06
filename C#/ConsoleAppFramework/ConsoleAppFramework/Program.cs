74f4664c0e9527f557dc99bc67a43437329e269f
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-01-06 11:55:37 +0100

    Exo 2.8

--- C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
+++ C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
@@ -10,16 +10,16 @@ namespace ConsoleAppFramework
     {
         static void Main(string[] args)
         {
-            int n, p;
-            //crée les entiers n, p et q
-            n = 10;
-            //affecte la valeur 10 dans la case mémoire de n
-            p = 4;
-            //affecte la valeur 5 dans la case mémoire de p
-            n = n * p;
-            //affecte la valeur de la multiplication de n*p dans la case mémoire de n
-            p = n / p;
-            //affecte la valeur de la division de n/p dans la case mémoire de n
+            double a, b, c;
+            //crée les réels a, b et c
+            a = 5.25;
+            //affecte la valeur 5.25 dans la case mémoire de a
+            b = 2.0 * a;
+            //affecte la valeur de la multiplication de 2.0*a dans la case mémoire de b
+            c = 1.5;
+            //affecte la valeur 1.5 dans la case mémoire de c
+            b = 5 * c;
+            //affecte la valeur de la multiplication de 5.*c dans la case mémoire de b
         }
     }
 }
