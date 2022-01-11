d72d08fca849da7cb43e69e6dbb8379de8ce3611
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-01-11 18:38:21 +0100

    7.4

--- C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
+++ C#/ConsoleAppFramework/ConsoleAppFramework/Program.cs
@@ -1,30 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleAppFramework
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] array1 = new int[8];
-            int i;
+            int [] myTab = InitTab();
 
-            array1[0] = 1;
-            array1[1] = 1;
+            int pmax = FindMax(myTab);
 
-            for (i = 2; i < array1.Length; i++)
-            {
-                array1[i] = array1[i-1] + array1[i - 2];               
-            }
+            /* monTypedObjet p= new monTypedObjet();
+             p.X = 100;
+             testO(p);
+             Console.WriteLine(p.X);*/
 
-            for (i = 0; i < array1.Length; i++)
-            {
-                Console.WriteLine(array1[i]);
-            }
+            string s = string.Format("{0} => {1}", pmax, myTab[pmax]);
+            Console.WriteLine(s);
+            
+            Console.WriteLine("max :{1} à la position :{0} ", pmax, myTab[pmax]);
+            Console.ReadLine();
         }
+
+        static int[] InitTab()
+        {
+            int[] array1 = new int[200];
+            array1[64] = 800;
+            return array1;
+        }
+
+        static int FindMax(int[] tab)
+        {
+            int i;
+            int max = tab[0];
+            int maxPos = 0;
+
+            for (i = 1; i < tab.Length; i++)
+            {
+                if (tab[i] > max)
+                {
+                    max = tab[i];
+                    maxPos = i;
+                }
+            }
+            return maxPos;
+        }
+        static void testO (monTypedObjet p)
+        {
+            p.X = 5;
+            p = null;
+        }
+
+
+    }
+
+    class monTypedObjet
+    {
+        public int X;
+        public int Y;
     }
 }
--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,13 @@
-let array1 = new Array(8);
+let array1 = new Array(200);
 let i;
+let max = array1[0];
+let maxPos = 0;
 
-array1[0] = 1;
-array1[1] = 1;
-
-for (i = 2; i < array1.length; i++) {
-  array1[i] = array1[i - 1] + array1[i - 2];
+for (i = 1; i < array1.length; i++) {
+  array1[i] = i;
+  if (array1[i] > max) {
+    max = array1[i];
+    maxPos = i;
+  }
 }
-
-for (i = 0; i < array1.length; i++) {
-  console.log(array1[i]);
-}
+console.log(maxPos);
