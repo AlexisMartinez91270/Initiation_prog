e515e5c0e3a40a23b2b92c8003a971670bce015b
Alexis Martinez <alexis.martinez@yahoo.fr>
2022-01-12 11:18:47 +0100

    7.4 V2 (Objet)

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,35 @@
-let array1 = new Array(200);
-let i;
-let max = array1[0];
-let maxPos = 0;
+"use strict";
 
-for (i = 1; i < array1.length; i++) {
-  array1[i] = i;
-  if (array1[i] > max) {
-    max = array1[i];
-    maxPos = i;
-  }
+let Main = function () {
+  let myTab = InitTab();
+
+  let maximum = FindMax(myTab);
+
+  let s = `${maximum.X} => ${maximum.Y}`;
+  console.log(s);
+
+  console.log(`max : ${maximum.X} à la position : ${maximum.Y}`);
+};
+
+function InitTab() {
+  let array1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  array1[12] = 800;
+  return array1;
 }
-console.log(maxPos);
+
+function FindMax(tab) {
+  let i;
+  let max = tab[0];
+  let maxPos = 0;
+
+  for (i = 1; i < tab.length; i++) {
+    if (tab[i] > max) {
+      max = tab[i];
+      maxPos = i;
+    }
+  }
+  let maximum = { X: max, Y: maxPos };
+  return maximum;
+}
+
+window.onload = Main();
